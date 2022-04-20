e96309888fb86a0ca02004c03762b7214618a866
Bamboo6 <dream1718@foxmail.com>
2022-04-20 15:19:28 +0800

    finished LabReports_3 v2.0

--- 2022/LabReports_3.cpp
+++ 2022/LabReports_3.cpp
@@ -9,25 +9,27 @@ using namespace std;
 
 //停车场最大容量
 #define MAX 5
+//车位没车辆时显示的占位符
+#define emptyPlaceholder "空"
 
 //时间结构体
 typedef struct time {
     string locTime;
-//    string hour;
-//    string min;
-//    string sec;
 } Time;
 
 //车辆信息结构体
 typedef struct node {
-    char num[10]{};
+    string num;
     Time arrive;
 //    Time leave;
 } CarNode;
 
 typedef struct NODE {
     CarNode *stack[MAX + 1];
+//    栈顶指针索引
     int top;
+//    真实车辆数
+    int realStackCount;
 } SeqStackCar;
 
 typedef struct car {
@@ -35,6 +37,7 @@ typedef struct car {
     struct car *next;
 } QueueNode;
 
+//便道队列
 typedef struct Node {
     QueueNode *head;
     QueueNode *rear;
@@ -43,7 +46,7 @@ typedef struct Node {
 //栈的初始化
 void InitStack(SeqStackCar *s) {
     int i;
-    s->top = 0;
+    s->top = s->realStackCount = 0;
     for (i = 0; i <= MAX; i++) {
         s->stack[s->top] = nullptr;
     }
@@ -51,14 +54,16 @@ void InitStack(SeqStackCar *s) {
 
 //队列的初始化
 int InitQueue(LinkQueueCar *Q) {
-    Q->head = (QueueNode *) malloc(sizeof(QueueNode));
-    if (Q->head != nullptr) {
-        Q->head->next = nullptr;
-        Q->rear = Q->head;
-        return (1);
-    } else {
-        return (-1);
-    }
+//    Q->head = (QueueNode *) malloc(sizeof(QueueNode));
+//    if (Q->head != nullptr) {
+//        Q->head->next = nullptr;
+//        Q->rear = Q->head;
+//        return (1);
+//    } else {
+//        return (-1);
+//    }
+    Q->head = Q->rear = nullptr;
+    return 1;
 }
 
 string nowTime() {
@@ -68,10 +73,10 @@ string nowTime() {
     nowTime[0] = to_string(ltm->tm_hour);
     nowTime[1] = to_string(ltm->tm_min);
     nowTime[2] = to_string(ltm->tm_sec);
-    for (auto & i : nowTime) {
+    for (auto &i: nowTime) {
         if (i.length() != 2) {
 //            i = "0" + i;
-            i.insert(0,"0");
+            i.insert(0, "0");
         }
     }
     int i = 0;
@@ -103,7 +108,7 @@ string nowTime() {
 //    cout << ltm->tm_sec << endl;
 }
 
-void arrive(SeqStackCar *Enter, LinkQueueCar *W) {
+void arrive(SeqStackCar *Enter, LinkQueueCar *W, SeqStackCar *Temp) {
     CarNode *p;
     QueueNode *t;
 //    p = (CarNode *) malloc(sizeof(CarNode));
@@ -111,32 +116,56 @@ void arrive(SeqStackCar *Enter, LinkQueueCar *W) {
     p = new CarNode;
     cout << "请输入车牌号:";
     cin >> p->num;
-    if (Enter->top < MAX) {
-        Enter->top++;
-        cout << "车辆在车场第 " << Enter->top << " 位置" << endl;
-        cout << "车辆入场时间为：";
-        string getTime = nowTime();
-//        const char *temp;
-//        p->arrive.locTime = strcpy(temp, getTime.c_str());
-//        cout << getTime;
-        p->arrive.locTime = getTime;
-        cout << p->arrive.locTime << endl;
-//        cin>>p->arrive.hour;
-//        cout<<getTime[1]<<"^^^^^^^^^^^^^^";
-//        p->arrive.hour = getTime[0];
-//        p->arrive.min = getTime[1];
-//        p->arrive.sec = getTime[2];
-//        cout << p->arrive.hour << ":"
-//             << p->arrive.min << ":"
-//             << p->arrive.sec << endl;
-        Enter->stack[Enter->top] = p;
+    if (Enter->realStackCount < MAX) {
+        //把要删除的车辆的前面的车开出来，进临时栈
+        while (Enter->stack[Enter->top] != nullptr && Enter->stack[Enter->top]->num != emptyPlaceholder) {
+            Temp->top++;
+            Temp->stack[Temp->top] = Enter->stack[Enter->top];
+            Enter->stack[Enter->top] = nullptr;
+            Enter->top--;
+        }
+        if (Enter->top == 0) {
+            //直接入栈，1，2，3满了，接下来入4
+            //1.先把临时栈里德车辆进停车场
+            while (Temp->top >= 1) {
+                Enter->top++;
+                Enter->stack[Enter->top] = Temp->stack[Temp->top];
+                Temp->stack[Temp->top] = nullptr;
+                Temp->top--;
+            }
+            //2.添加车辆
+            Enter->top++;
+            Enter->stack[Enter->top] = p;//加入
+            p->arrive.locTime = nowTime();
+            cout << "车辆在车场第 " << Enter->top << " 位置" << endl;
+            cout << "车辆入场时间为：" << p->arrive.locTime;
+        } else {
+            //情况：1,空,3,4，进入2
+            //1.先添加车辆
+            Enter->stack[Enter->top] = p;//加入
+            p->arrive.locTime = nowTime();
+            cout << "车辆在车场第 " << Enter->top << " 位置" << endl;
+            cout << "车辆入场时间为：" << p->arrive.locTime;
+            //2.先把临时栈里德车辆进停车场
+            while (Temp->top >= 1) {
+                Enter->top++;
+                Enter->stack[Enter->top] = Temp->stack[Temp->top];
+                Temp->stack[Temp->top] = nullptr;
+                Temp->top--;
+            }
+        }
+        Enter->realStackCount++;
     } else {
         cout << "停车场已满，请进入便道等待。";
-        t = (QueueNode *) malloc(sizeof(QueueNode));
+        t = new QueueNode;
         t->data = p;
         t->next = nullptr;
-        W->rear->next = t;
-        W->rear = t;
+        if (W->head == nullptr) {
+            W->head = W->rear = t;
+        } else {
+            W->rear->next = t;
+            W->rear = t;
+        }
     }
 }
 
@@ -163,33 +192,38 @@ void parkingLotList(SeqStackCar *S) {
 void temporaryDrivewayList(LinkQueueCar *W) {
     QueueNode *p;
     int i;
-    p = W->head->next;
-    if (W->head != W->rear) {
+    p = W->head;
+    if (p == nullptr) {
+        cout << "便道没有车。" << endl;
+    } else {
         cout << "等待车辆的车牌号：" << endl;
         for (i = 1; (p != nullptr); i++) {
             cout << "第 " << i << " 辆车：";
             cout << p->data->num << endl;
             p = p->next;
         }
-    } else {
-        cout << "便道没有车。" << endl;
     }
 }
 
-void leave(SeqStackCar *Enter, SeqStackCar *Temp, LinkQueueCar *W) {
+//车辆离开登记
+void leave(SeqStackCar *Enter, LinkQueueCar *W, SeqStackCar *Temp) {
 //    车辆的离开
     int room;
-    CarNode *p, *t;
-    QueueNode *q;
+    CarNode *p;
 //    判断停车场是否为空
-    if (Enter->top > 0) {
+    if (Enter->realStackCount > 0) {
         while (true) {
             cout << "请输入车的位置（1--" << Enter->top << "）：";
             cin >> room;
-            if (room >= 1 && room <= Enter->top) {
+            if (room >= 1 && room <= Enter->top && Enter->stack[room]->num != emptyPlaceholder) {
+                cout << "车牌号为 " << Enter->stack[room]->num << " 的车辆已开出停车场。" << endl;
                 break;
             } else {
-                cout << "输入有误，请重新输: ";
+                if (Enter->stack[room]->num == emptyPlaceholder) {
+                    cout << "该车位没有车，请重新输入：";
+                } else {
+                    cout << "输入有误，请重新输入: ";
+                }
             }
         }
 //        把要删除的车辆的前面的车开出来，进临时栈
@@ -199,10 +233,23 @@ void leave(SeqStackCar *Enter, SeqStackCar *Temp, LinkQueueCar *W) {
             Enter->stack[Enter->top] = nullptr;
             Enter->top--;
         }
-//        把要删除的车辆节点赋给p。
-        p = Enter->stack[Enter->top];
-        Enter->stack[Enter->top] = nullptr;
-        Enter->top--;
+        p = Enter->stack[Enter->top];//需要离开的车辆
+        p->num = emptyPlaceholder;
+        p->arrive.locTime = "";
+        Enter->realStackCount--;//真实车辆数 - 1
+        if (W->head == nullptr) {
+//            便道没有车辆
+        } else {
+//            便道有车辆
+//            真实车辆数 + 1
+            Enter->realStackCount++;
+//            cout << W->head->data->num << endl;
+            Enter->stack[Enter->top] = W->head->data;
+            W->head = W->head->next;
+            Enter->stack[Enter->top]->arrive.locTime = nowTime();
+            cout << "车牌号为 " << Enter->stack[Enter->top]->num << " 的车辆进入停车场的 " << Enter->top << " 号车位。" << endl;
+            cout << "车辆入场时间为：" << Enter->stack[Enter->top]->arrive.locTime << endl;
+        }
 //        再把临时栈里德车辆进停车场
         while (Temp->top >= 1) {
             Enter->top++;
@@ -210,36 +257,6 @@ void leave(SeqStackCar *Enter, SeqStackCar *Temp, LinkQueueCar *W) {
             Temp->stack[Temp->top] = nullptr;
             Temp->top--;
         }
-//        如果便道上有车，则再开进停车场。
-        if ((W->head != W->rear) && Enter->top < MAX) {
-            q = W->head->next;
-            t = q->data;
-            Enter->top++;
-            cout << "车牌号为 " << t->num << " 的车辆进入停车场的 " << Enter->top << " 号车位。" << endl;
-//            cout << "请输入进入停车场的时间：";
-//            cin >> t->arrive.hour >> t->arrive.min;
-            cout << "车辆入场时间为：";
-//            string *getTime = nowTime();
-//            t->arrive.hour = *getTime;
-//            t->arrive.min = *(getTime + 1);
-//            t->arrive.sec = *(getTime + 2);
-//            cout << t->arrive.hour << ":"
-//                 << t->arrive.min << ":"
-//                 << t->arrive.sec << endl;
-//            string getTime = nowTime();
-            t->arrive.locTime = nowTime();
-            cout << t->arrive.locTime << endl;
-            W->head->next = q->next;
-            if (q == W->rear) {
-                W->rear = W->head;
-            }
-            Enter->stack[Enter->top] = t;
-            free(q);
-        } else {
-            cout << "便道没有车。";
-        }
-    } else {
-        cout << "停车场没有车。";
     }
 }
 
@@ -270,21 +287,22 @@ enum operationCode {
 
 int main() {
     int operationCode;
-    int isExsit = 0;
+    int isExist = 0;
+//    停车场栈、临时栈
     SeqStackCar Enter, Temp;
+//    便道队列
     LinkQueueCar Wait;
-    int ch;
     InitStack(&Enter);
     InitStack(&Temp);
     InitQueue(&Wait);
-    while (!isExsit) {
+    while (!isExist) {
         operationCode = menu();
         switch (operationCode) {
             case o_exit:
-                isExsit = -1;
+                isExist = -1;
                 break;
             case o_arrive:
-                arrive(&Enter, &Wait);
+                arrive(&Enter, &Wait, &Temp);
                 break;
             case o_parkingLotList:
                 parkingLotList(&Enter);
@@ -293,7 +311,7 @@ int main() {
                 temporaryDrivewayList(&Wait);
                 break;
             case o_leave:
-                leave(&Enter, &Temp, &Wait);
+                leave(&Enter, &Wait, &Temp);
                 break;
             default:
                 cout << "输入有误！" << endl;
